7143908455a27468ead656c930094b6516b076cb
AlenaLis <lisovenko.al@justiceteam-it.com>
2021-08-09 18:27:18 +0300

    my prog

---
+++ src/Calendar/Calendar.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import {useState} from "react";
+import {Calendar, momentLocalizer} from 'react-big-calendar';
+import moment from 'moment';
+import 'moment/locale/ru';
+import {Link, Redirect} from 'react-router-dom';
+
+import './Calendar.css';
+
+const BigCalendar = () => {
+
+
+  const [token, setToken] = useState(localStorage.getItem('token'));
+  const [admin, setAdmin] = useState(localStorage.getItem('admin'));
+
+  const [myEvents, setMyEvents] = useState([
+    {
+      start: '',
+      end: ' ',
+      title: ''
+    }
+  ]);
+
+  const handleSelect = ({ start, end }) => {
+    const title =  prompt('New Event name');
+    if (title) {
+      let newEvent = {
+        start: start,
+        end: end,
+        title: title
+      }
+      setMyEvents([...myEvents, newEvent]);
+    }
+  };
+
+  const islogout = () => {
+    localStorage.setItem('token', JSON.stringify([]))
+    localStorage.setItem('admin', JSON.stringify([]))
+    localStorage.removeItem('token', JSON.stringify([]))
+    localStorage.removeItem('admin', JSON.stringify([]))
+    window.location.reload();
+  };
+
+  const localizer = momentLocalizer(moment);
+
+  const moveEvent = ({ event, start, end }) => {
+    const thisEvent = event;
+
+    const nextEvents = myEvents.map((existingEvent) => {
+      return existingEvent.id == event.id
+        ? { ...existingEvent, start, end }
+        : existingEvent;
+    });
+    setMyEvents(nextEvents);
+  };
+
+  return (
+    <div>
+
+      <div className="header_buttons">
+        <div className="button_register">
+          <Link to='/register/'>
+            Зарегистрировать новый аккаунт
+          </Link>
+        </div>
+        <div className="button_login">
+          <button
+            onClick={islogout}
+          >
+            Выйти из аккаунта
+          </button>
+        </div>
+      </div>
+      {admin == 1?
+        (
+          <div>
+            <button onClick={handleSelect}>Create event</button>
+          </div>
+        )
+        :
+        ''
+      }
+      <Calendar
+        localizer={localizer}
+        events={myEvents}
+        startAccessor="start"
+        endAccessor="end"
+        style={{height: 500}}
+        messages={{
+          next: ">",
+          previous: "<",
+          today: "Сегодня",
+          month: "Месяц",
+          week: "Неделя",
+          day: "День",
+        }}
+        views={['day', 'week', 'month']}
+        onSelectSlot={handleSelect}
+        popup={true}
+        tooltipAccessor={(e) => e.title}
+
+      />
+      {!token && <Redirect to="/login/"/>}
+    </div>
+  );
+}
+
+export default BigCalendar;
---
+++ src/services/index.js
@@ -0,0 +1,55 @@
+import {ajaxWrapper} from '../helpers/ajaxWrapper';
+
+import {urls} from '../helpers/constant';
+
+export const login = (data) => {
+  const url = `${urls.USER}/login`;
+  return ajaxWrapper({
+    method: 'POST',
+    url,
+    data,
+  }).then(data => data.data)
+};
+
+export const registration = (data) => {
+  const url = `${urls.USER}/register`;
+  return ajaxWrapper({
+    method: 'POST',
+    url,
+    data,
+  });
+};
+
+export const getAllArticles = () => {
+  const url = `${urls.CATEGORY}`;
+  return ajaxWrapper({
+    method: 'GET',
+    url,
+  }).then(data => data.data)
+};
+
+export const getOneArticle = (data, id) => {
+  const url = `${urls.CATEGORY}/${id}`;
+  return ajaxWrapper({
+    method: 'GET',
+    url,
+    data,
+  }).then(data => data.data)
+};
+
+export const getOneArticleById = (id) => {
+  const url = `${urls.CATEGORY}/article/${id}`;
+  return ajaxWrapper({
+    method: 'GET',
+    url,
+  }).then(data => data.data)
+};
+
+export const addOneArticle = (data) => {
+  const url = `${urls.CATEGORY}`;
+  return ajaxWrapper({
+    method: 'POST',
+    url,
+    data
+  }).then(data => data.data)
+};
